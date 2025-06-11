533bfe2a28efdf54223ab41b3f85767e74b0a46e
Daniel Chae <daniel.jiwoong@gmail.com>
2025-06-11 14:44:15 +0900

    theme in local storage
    types are still wonky

--- client/src/context/ThemeProvider.tsx
+++ client/src/context/ThemeProvider.tsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import type { ReactNode } from "react";
 import { ThemeContext } from "./ThemeContext";
 import type { ThemeOptions } from "./ThemeContext";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 interface ThemeProviderProps {
   children: ReactNode;
 }
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [options, setOptions] = useState<ThemeOptions>({ mode: "light" });
+  const [options, setOptions] = useLocalStorage<ThemeOptions>("theme-options", {
+    mode: "light",
+  });
   const toggleMode = () => {
     setOptions((o) => ({ ...o, mode: o.mode === "light" ? "dark" : "light" }));
   };
---
+++ client/src/hooks/useLocalStorage.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect, useCallback } from "react";
+
+const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [value, setValue] = useState<T>(initialValue);
+
+  const get = useCallback(() => {
+    try {
+      const value = window.localStorage.getItem(key);
+
+      return typeof value === "string" ? JSON.parse(value) : value;
+    } catch (e) {
+      console.log(e);
+    }
+  }, [key]);
+
+  const set = useCallback(
+    (newValue: T) => {
+      try {
+        window.localStorage.setItem(key, JSON.stringify(newValue));
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
+    [key],
+  );
+
+  useEffect(() => {
+    set(value);
+  }, [set, value]);
+
+  useEffect(() => {
+    setValue(get());
+  }, [get]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
